53e1fd58e2f245b56092e47d7896d701ef440a67
LeoTGx <leotgx@gmail.com>
2019-01-14 21:38:54 -0300

    Código do transmissor pronto pra teste

--- SSL-F180/Controle/Códigos Principais/Novos códigos/CodigoTransmissor/CodigoTransmissor.ino
+++ SSL-F180/Controle/Códigos Principais/Novos códigos/CodigoTransmissor/CodigoTransmissor.ino
@@ -1,8 +1,12 @@
 #include <SerialCommunication.h>
 #include <Radio.h>
+#include <FastCRC.h>
+
 
 #define tempoDelay 1000
 #define tempoDelay2 1090
+
+FastCRC16 CRC16;
 
 SerialCommunication serial(38400);
 Radio radio(0, 109, "TX");            //Módulo, canal, modo
@@ -12,29 +16,38 @@ void setup(){
   radio.setup();
 }
 
-//iasjdasd9asjijdasasoin
-
 bool isChecksumOk(char msg[]) {
   return (msg[6] == (msg[0] ^ msg[1] ^ msg[2] ^ msg[3] ^ msg[4] ^ msg[5]));
 }
 
 void loop(){
+  uint8_t crc;
   static unsigned long tempo = millis();
-  char msg[7]; // message size  + 1 (checksum)      
+  uint8_t msg[7]; // message size  + 1 (checksum) 
+  char msg_c[9];
 
-  if(serial.read(msg)){
+  for(int i; i<sizeof(msg); i++){ //Copiamos a mensagem para um vetor char.
+      msg_c[i] = msg[i];
+  }
+
+  crc = CRC16.xmodem(msg, sizeof(msg));
+
+  msg_c[7] = crc/256;
+  msg_c[8] = crc%256;
+  
+  if(serial.read(msg_c)){
     //if(millis() - tempo >= tempoDelay && millis() - tempo <= tempoDelay2)
     //  msg[1] |= 0x80;
     //else if(millis() - tempo > tempoDelay2)
     //  tempo = millis();
     //else
-    //  msg[1] &= 0x7F;
-    if(msg[0] == START && msg[5] == FINAL && isChecksumOk(msg)) {
-      radio.send(true, msg);
+    //  msg[1] &= 0x7F; 
+    if(msg_c[0] == START && msg_c[5] == FINAL && isChecksumOk(msg_c)) {
+      radio.send(true, msg_c);
     }      
   }
 
   else{
-    radio.send(false, msg); 
+    radio.send(false, msg_c); 
   }
 }
